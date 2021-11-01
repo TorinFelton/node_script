44a0fdfb00236d1cc49c985ac6498bb2b3e62fda
Torin Felton <torinbrody1@gmail.com>
2021-11-01 18:16:33 +0000

    update primary parse reference to secondary

--- node_script/Parser/SecondaryParsers/ControlFlow.cs
+++ node_script/Parser/SecondaryParsers/ControlFlow.cs
@@ -9,7 +9,7 @@ namespace node_script.Parser.SecondaryParsers
 {
     static class ControlFlow
     {
-        public static List<Func<List<Token>, List<Step>, bool>> ControlFlowParsers = new List<Func<List<Token>, List<Step>, bool>>() // "List of functions that take arguments: List<Token>, List<Step> and return a bool
+        public static List<Func<List<Step>, bool>> ControlFlowParsers = new List<Func<List<Step>, bool>>() // "List of functions that take arguments: List<Token>, List<Step> and return a bool
         // List of variable-related syntax parsing functions.
         {
             // This list will grow as I add more syntax variants for parsing anything to do with variables.
@@ -17,13 +17,13 @@ namespace node_script.Parser.SecondaryParsers
         };
         public static bool TryParseControlFlow(List<Token> tokens, List<Step> steps)
         {
-            return ParserTools.TryPrimaryParse(ControlFlowParsers, tokens, steps);
+            return ParserTools.TrySecondaryParse(ControlFlowParsers, steps);
         }
 
-        public static bool WhileLoopParser(List<Token> tokens, List<Step> steps)
+        public static bool WhileLoopParser(List<Step> steps)
         {
             // we don't actually need the List<Token> but for using the dynamic parsing (ParserTools.TryParse) we have defined them all to take it as an argument. 
-            foreach ()
+            
         }
     }
 }
